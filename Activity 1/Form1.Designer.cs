a445f44dd37eb3a31d3cd8fdde2050b36e484c9d
peter <peter@DESKTOP-2DVQ078>
2022-05-22 00:04:08 -0500

    Add project files.

---
+++ Activity 1/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Activity_1
+{
+    partial class Activity1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Close = new System.Windows.Forms.Button();
+            this.TitleBox = new System.Windows.Forms.Label();
+            this.OutputBox = new System.Windows.Forms.TextBox();
+            this.OutputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Close
+            // 
+            this.Close.Location = new System.Drawing.Point(301, 231);
+            this.Close.Name = "Close";
+            this.Close.Size = new System.Drawing.Size(75, 23);
+            this.Close.TabIndex = 0;
+            this.Close.Text = "Close";
+            this.Close.UseVisualStyleBackColor = true;
+            this.Close.Click += new System.EventHandler(this.Close_Click);
+            // 
+            // TitleBox
+            // 
+            this.TitleBox.AutoSize = true;
+            this.TitleBox.Location = new System.Drawing.Point(298, 140);
+            this.TitleBox.Name = "TitleBox";
+            this.TitleBox.Size = new System.Drawing.Size(27, 13);
+            this.TitleBox.TabIndex = 1;
+            this.TitleBox.Text = "Title";
+            // 
+            // OutputBox
+            // 
+            this.OutputBox.AcceptsReturn = true;
+            this.OutputBox.Location = new System.Drawing.Point(301, 156);
+            this.OutputBox.Name = "OutputBox";
+            this.OutputBox.Size = new System.Drawing.Size(100, 20);
+            this.OutputBox.TabIndex = 2;
+            this.OutputBox.TextChanged += new System.EventHandler(this.OutputBox_TextChanged);
+            // 
+            // OutputLabel
+            // 
+            this.OutputLabel.AutoSize = true;
+            this.OutputLabel.Location = new System.Drawing.Point(301, 186);
+            this.OutputLabel.Name = "OutputLabel";
+            this.OutputLabel.Size = new System.Drawing.Size(0, 13);
+            this.OutputLabel.TabIndex = 3;
+            // 
+            // Activity1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.OutputLabel);
+            this.Controls.Add(this.OutputBox);
+            this.Controls.Add(this.TitleBox);
+            this.Controls.Add(this.Close);
+            this.Name = "Activity1";
+            this.Text = "Activity 1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Close;
+        private System.Windows.Forms.Label TitleBox;
+        private System.Windows.Forms.TextBox OutputBox;
+        private System.Windows.Forms.Label OutputLabel;
+    }
+}
+
---
+++ Activity 1/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity_1
+{
+    public partial class Activity1 : Form
+    {
+        public Activity1()
+        {
+            InitializeComponent();
+        }
+
+        private void Close_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void OutputBox_TextChanged(object sender, EventArgs e)
+        {
+            OutputLabel.Text = OutputBox.Text;
+        }
+    }
+}
